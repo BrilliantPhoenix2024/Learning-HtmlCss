aa4c75466387dd1fbf42fa2c970c3bb797607ba4
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-09 19:56:46 +0330

    Adding an Event Listener

--- Formatting_with_CSS/Working with JavaScript CSS/Main Project/shared.js
+++ Formatting_with_CSS/Working with JavaScript CSS/Main Project/shared.js
@@ -1,3 +1,10 @@
 const backdrop = document.querySelector('.backdrop');
+const selectPlanButtons = document.querySelectorAll('.plan button');
+const modal = document.querySelector('.modal');
 
-backdrop.style.display = 'block';
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function () {
+    modal.style.display = 'block';
+    backdrop.style.display = 'block';
+  });
+}
