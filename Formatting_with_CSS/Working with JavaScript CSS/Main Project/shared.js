03a2d168eba622b3c3a99889d7f63ae77814e1d6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-09 20:02:00 +0330

    Adding Styles with JavaScript

--- Formatting_with_CSS/Working with JavaScript CSS/Main Project/shared.js
+++ Formatting_with_CSS/Working with JavaScript CSS/Main Project/shared.js
@@ -1,10 +1,20 @@
 const backdrop = document.querySelector('.backdrop');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const modal = document.querySelector('.modal');
+const modalNotButton = document.querySelector('.modal__action--negative');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function () {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
   });
+}
+
+backdrop.addEventListener('click', closeModal);
+
+modalNotButton.addEventListener('click', closeModal);
+
+function closeModal() {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
 }
