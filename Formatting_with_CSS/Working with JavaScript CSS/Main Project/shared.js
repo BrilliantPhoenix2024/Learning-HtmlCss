3fa288b3abb907c0088b66895fb9c1e8965d8291
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-04-13 17:07:28 +0330

    Understanding Property Notations

--- Formatting_with_CSS/Working with JavaScript CSS/Main Project/shared.js
+++ Formatting_with_CSS/Working with JavaScript CSS/Main Project/shared.js
@@ -5,6 +5,10 @@ const modalNoButton = document.querySelector('.modal__action--negative');
 const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
 
+console.dir(backdrop);
+console.dir(backdrop.style.backgroundImage);
+console.dir(backdrop.style['background-image']);
+
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function () {
     // modal.style.display = 'block';
