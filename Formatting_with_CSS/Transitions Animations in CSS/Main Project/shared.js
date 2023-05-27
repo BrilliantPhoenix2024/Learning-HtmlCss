8b59b6a4836e6529ff5c9c0204fc38aee5dc2c67
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-05-28 00:06:08 +0330

    Start 'Transitions  Animations in CSS' Module

---
+++ Formatting_with_CSS/Transitions Animations in CSS/Main Project/shared.js
@@ -0,0 +1,46 @@
+const backdrop = document.querySelector('.backdrop');
+const selectPlanButtons = document.querySelectorAll('.plan button');
+const modal = document.querySelector('.modal');
+const modalNoButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
+// console.dir(backdrop);
+// console.dir(backdrop.style.backgroundImage);
+// console.dir(backdrop.style['background-image']);
+
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function () {
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.className = 'open'; // this will actually overwrite the complete class list.
+    modal.classList.add('open');
+    backdrop.classList.add('open');
+  });
+}
+
+backdrop.addEventListener('click', function () {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+  // backdrop.style.display = 'none';
+  // modal.style.display = 'none';
+  if (modal) {
+    modal.classList.remove('open');
+  }
+  backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function () {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+});
