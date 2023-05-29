ca5e40747634f02b7a28c1724ca68b2d6107aa31
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-05-29 15:42:04 +0330

    Transitions display

--- Formatting_with_CSS/Transitions Animations in CSS/Main Project/shared.js
+++ Formatting_with_CSS/Transitions Animations in CSS/Main Project/shared.js
@@ -15,7 +15,10 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
     // backdrop.style.display = 'block';
     // modal.className = 'open'; // this will actually overwrite the complete class list.
     modal.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(function () {
+      backdrop.classList.add('open');
+    }, 10);
   });
 }
 
@@ -36,11 +39,16 @@ function closeModal() {
     modal.classList.remove('open');
   }
   backdrop.classList.remove('open');
+  setTimeout(function () {
+    backdrop.style.display = 'none';
+  }, 200);
 }
 
 toggleButton.addEventListener('click', function () {
   // mobileNav.style.display = 'block';
-  // backdrop.style.display = 'block';
   mobileNav.classList.add('open');
-  backdrop.classList.add('open');
+  backdrop.style.display = 'block';
+  setTimeout(function () {
+    backdrop.classList.add('open');
+  }, 10);
 });
