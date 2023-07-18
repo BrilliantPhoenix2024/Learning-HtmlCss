4c52fa792970a1381b506cf687229e688f2221d1
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-07-18 23:12:48 +0330

    Nav Button

--- The_killer_website_project/TeaStation/app.js
+++ The_killer_website_project/TeaStation/app.js
@@ -3,13 +3,13 @@ const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
 // Show Nav
-// navBtn.addEventListener("click", () => {
-//   navbar.classList.add("showNav");
-// });
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
 // Close Nav
-// navClose.addEventListener("click", () => {
-//   navbar.classList.remove("showNav");
-// });
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
 
 // Setup date
 const date = document.getElementById("date");
