4645f43d3d550c7afcb306a650efafe0b64f9227
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-07-18 22:21:13 +0330

    Footer Html

--- The_killer_website_project/TeaStation/app.js
+++ The_killer_website_project/TeaStation/app.js
@@ -1,11 +1,16 @@
+// Setup Nav
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+// Show Nav
+// navBtn.addEventListener("click", () => {
+//   navbar.classList.add("showNav");
+// });
+// Close Nav
+// navClose.addEventListener("click", () => {
+//   navbar.classList.remove("showNav");
+// });
 
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
-
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
-});
+// Setup date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
